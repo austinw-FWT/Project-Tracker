7ba67f6afc9edb882bab08e2252b8a3337b9f84b
austinw-FWT <austinw@farwesttechnologies.com>
2026-03-31 15:21:13 -0700

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAAFCbuRe4lzdE4OUE_ulALZCN6EfcDNIQ",
+  authDomain: "fwt-lv-tracker.firebaseapp.com",
+  databaseURL: "https://fwt-lv-tracker-default-rtdb.firebaseio.com",
+  projectId: "fwt-lv-tracker",
+  storageBucket: "fwt-lv-tracker.firebasestorage.app",
+  messagingSenderId: "670069354019",
+  appId: "1:670069354019:web:26800dd2423cf44b334b96",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+export {
+  auth,
+  googleProvider,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  onAuthStateChanged,
+  signOut,
+  updateProfile,
+};
